fa8ca4b1895e8f0b787485f3af06908200c632e2
kobebryan9 <36723798+kobebryan9@users.noreply.github.com>
2021-09-25 21:58:47 +0800

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -28,6 +28,7 @@ def implement(room, grid, ind):
     m = len(grid[0])    
     big = n*m + 1
     grid1 = b = [x[:] for x in grid]
+    grid2 = b = [x[:] for x in grid]    
 
     stx = 0
     sty = 0
@@ -129,6 +130,103 @@ def implement(room, grid, ind):
         ans = -1
     answer["p3"] = ans       
 
+    
+    for i in range(n):
+        for j in range(m):
+            grid1[i][j] = -1
+            if (grid[i][j] == 2):
+                grid[i][j] = 0
+
+    tot = 0
+
+    for i in range(n):
+        for j in range(m):
+            if (grid[i][j] != 0):
+                if (grid1[i][j] == -1):
+                        q = deque()
+                        q.append([i,j])
+                        grid1[i][j] = tot
+                        tot = tot + 1
+                        while (len(q) > 0):
+                            g = q.popleft()
+                            xx = g[0]
+                            yy = g[1]
+                            for ii in range(4):
+                                tmx = xx + dx[ii]
+                                tmy = yy + dy[ii]
+                                if check(tmx,tmy,n,m) == 1:
+                                    print("testing", tmx,tmy,grid1[tmx][tmy],grid1[xx][yy],grid[tmx][tmy] )
+                                    if grid1[tmx][tmy] != grid1[xx][yy] and (grid[tmx][tmy] != 0):
+                                        grid1[tmx][tmy] = grid1[xx][yy]
+                                        q.append([tmx,tmy])
+
+    w, h = tot, tot
+    cost = [[0 for x in range(w)] for y in range(h)] 
+    for i in range(tot):
+        for j in range(tot):
+            cost[i][j] = big
+
+    for i in range(n):
+        for j in range(m):
+            team = grid1[i][j]
+            if (team == -1):
+                continue
+            if (cost[team][team] == big):
+                print("team",i,j,team)
+                cost[team][team] = 0
+                q = deque()                
+                for ii in range(n):
+                    for jj in range(m):
+                        grid2[ii][jj] = big
+                        if (grid1[ii][jj] == team):
+                            grid2[ii][jj] = 0
+                            q.append([ii,jj])
+
+                while (len(q) > 0):
+                    g = q.popleft()
+                    xx = g[0]
+                    yy = g[1]
+                    for ii in range(4):
+                        tmx = xx + dx[ii]
+                        tmy = yy + dy[ii]
+                        if check(tmx,tmy,n,m) == 1:
+                            #print("testing", tmx,tmy,grid2[tmx][tmy],grid1[xx][yy],grid[tmx][tmy] )
+                            if grid2[tmx][tmy] > grid2[xx][yy] + 1 and (grid[tmx][tmy] == 0):
+                                grid2[tmx][tmy] = grid2[xx][yy] + 1
+                                q.append([tmx,tmy])
+                            if grid2[tmx][tmy] > grid2[xx][yy] and (grid[tmx][tmy] != 0):
+                                grid2[tmx][tmy] = grid2[xx][yy]
+                                q.append([tmx,tmy])                            
+
+                for ii in range(n):
+                    for jj in range(m):
+                        cost[team][grid1[ii][jj]] = min(cost[team][grid1[ii][jj]],grid2[ii][jj])
+
+
+    dist = [0 for x in range(tot)]
+    cur = [0 for x in range(tot)]
+    for x in range(tot):
+        dist[x] = cost[0][x]
+        cur[x] = 0
+
+    cur[0] = 1
+    ans = 0
+
+    for i in range(tot-1):
+        mim = big
+        id = 0
+        for j in range(tot):
+            if (id == 0) and j != 0 and dist[j] < mim and cur[j] == 0:
+                mim = dist[j]
+                id = j
+
+        cur[id] = 1
+        ans += mim
+        for j in range(tot):
+            if cur[j] == 0:
+                dist[j] = min(dist[j],cost[id][j])
+
+    answer["p4"] = ans
     return answer
 
 def check(x, y, n, m):
