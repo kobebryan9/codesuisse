a78b9d8ee9368ab45ae8fbf2d950374db885708a
Fuzen527 <65777400+Fuzen527@users.noreply.github.com>
2021-09-26 05:07:42 +0800

    Update swissStig.py

--- codeitsuisse/routes/swissStig.py
+++ codeitsuisse/routes/swissStig.py
@@ -28,46 +28,46 @@ def implement(questions, coefficients, maxRating):
         mx.append(-1)
         mn.append(1e9)
     for i in reversed(range(Q)):
-        mn[i] = min(mn[i + 1], questions[i]["from"])
-        mx[i] = max(mx[i + 1], questions[i]["to"])
+        mn[i] = min(mn[i + 1], questions[i][0])
+        mx[i] = max(mx[i + 1], questions[i][1])
     for i in range(Q - 1):
-        if (mn[i + 1] >= questions[i]["from"] and mx[i + 1] <= questions[i]["to"]):
-            questions[i]["here"] = 1
+        if (mn[i + 1] >= questions[i][0] and mx[i + 1] <= questions[i][1]):
+            questions[i][2] = 1
             correct += 1
         else:
-            questions[i]["here"] = 0
+            questions[i][2] = 0
     total = 0
     if (correct == 0):
         questions.sort()
         total = 0
         last = 0
         for i in range(Q - 1):
-            if (questions[i]["from"] > last):
-                total += questions[i]["from"] - last - 1
-                last = questions[i]["to"]
+            if (questions[i][0] > last):
+                total += questions[i][0] - last - 1
+                last = questions[i][1]
             else:
-                last = max(last, questions[i]["to"])
+                last = max(last, questions[i][1])
         total += maxRating - last
     else:
         pl = 0
         last = 0
         end = 0
         for i in range(Q - 1):
-            if (questions[i]["here"] == 1):
+            if (questions[i][2] == 1):
                 pl = i
-                last = questions[i]["from"] - 1
-                end = questions[i]["to"]
+                last = questions[i][0] - 1
+                end = questions[i][1]
                 break
         remain = []
         for i in range(i + 1, Q - 1):
             remain.append(questions[i])
         remain.sort()
         for q in remain:
-            if (q["from"] > last):
-                total += q["from"] - last - 1
-                last = q["to"]
+            if (q[0] > last):
+                total += q0] - last - 1
+                last = q[1]
             else:
-                last = max(last, q["to"])
+                last = max(last, q[1])
         total += end - last
     result = []
     result["p"] = 1
