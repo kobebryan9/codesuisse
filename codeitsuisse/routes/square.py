839b57de08830d42804212685cf2ab33c775348c
kobebryan9 <36723798+kobebryan9@users.noreply.github.com>
2021-09-25 14:54:54 +0800

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -12,7 +12,7 @@ def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue
+    result = inputValue * inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
